df242a5010613ced6965befcd33db40d04e77089
Fabian Celdeiro <celdeiro@gmail.com>
2015-03-27 11:29:59 -0300

    open tok

---
+++ OpenTokDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenTokDemo
+//
+//  Created by Fabian Celdeiro on 3/26/15.
+//  Copyright (c) 2015 Fabian Celdeiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenTokDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenTokDemo
+//
+//  Created by Fabian Celdeiro on 3/26/15.
+//  Copyright (c) 2015 Fabian Celdeiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
